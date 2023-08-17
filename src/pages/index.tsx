53902da9347ad0863fc51cb760632f8548f215b1
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-17 22:11:31 +0900

    Update layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 
+import styles from '@/styles/Home.module.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -12,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${inter.className}`}>Hello</main>
+      <div className={styles.container}>
+        <main className={`${styles.main} ${inter.className}`}>Hello</main>
+      </div>
     </>
   );
 }
