0bf1835a311e05b77e2b57d67944c3c23c2a1e4c
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-17 21:45:51 +0900

    Add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next/core-web-vitals', // next strict.
+    'plugin:prettier/recommended', // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['unused-imports', 'simple-import-sort'],
+  rules: {
+    'unused-imports/no-unused-imports': 'warn',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
+};
