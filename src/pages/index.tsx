6aab8c8cce8fef32a612bccc3cd068e35214f740
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-18 18:10:28 +0900

    Test 100vh 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <div>111</div>
           <div>222</div>
           <div>333</div>
-          <div>111</div>
+          {/* <div>111</div>
           <div>222</div>
           <div>333</div>
           <div>111</div>
@@ -57,7 +57,7 @@ export default function Home() {
           <div>333</div>
           <div>111</div>
           <div>222</div>
-          <div>333</div>
+          <div>333</div> */}
         </main>
       </div>
     </>
