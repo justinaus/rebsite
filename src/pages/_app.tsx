3417491add8b4ab9db3f742b3aff7aaa860d0e12
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-29 18:36:13 +0900

    Add users page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 
 import AppLayout from '@/components/AppLayout';
@@ -8,12 +9,17 @@ import { suitRegular } from '@/fonts/suitRegular';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <AppLayout>
-        <main className={suitRegular.className}>
-          <Component {...pageProps} />
-        </main>
-      </AppLayout>
-    </RecoilRoot>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <RecoilRoot>
+        <AppLayout>
+          <main className={suitRegular.className}>
+            <Component {...pageProps} />
+          </main>
+        </AppLayout>
+      </RecoilRoot>
+    </>
   );
 }
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Users() {
+  return (
+    <>
+      <Head>
+        <title>Users</title>
+        <meta name="description" content="Users" />
+      </Head>
+      <div>Users</div>
+    </>
+  );
+}
