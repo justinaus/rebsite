33b4c6516c19cd5ae1119d0729977c60193b7f3f
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-29 16:37:05 +0900

    Add useIntersection hook

---
+++ src/hooks/useIntersection.ts
@@ -0,0 +1,32 @@
+// https://github.com/streamich/react-use/blob/master/src/useIntersection.ts
+
+import { RefObject, useEffect, useState } from 'react';
+
+const useIntersection = (
+  ref: RefObject<HTMLElement>,
+  options: IntersectionObserverInit,
+): IntersectionObserverEntry | null => {
+  const [intersectionObserverEntry, setIntersectionObserverEntry] =
+    useState<IntersectionObserverEntry | null>(null);
+
+  useEffect(() => {
+    if (ref.current && typeof IntersectionObserver === 'function') {
+      const handler = (entries: IntersectionObserverEntry[]) => {
+        setIntersectionObserverEntry(entries[0]);
+      };
+
+      const observer = new IntersectionObserver(handler, options);
+      observer.observe(ref.current);
+
+      return () => {
+        setIntersectionObserverEntry(null);
+        observer.disconnect();
+      };
+    }
+    return () => {};
+  }, [options, ref]);
+
+  return intersectionObserverEntry;
+};
+
+export default useIntersection;
