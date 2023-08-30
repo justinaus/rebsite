0a73b5409aa9893fab5fd1ca538225d6d4f4d559
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-08-30 13:43:08 +0900

    Update users - suspense

---
+++ src/components/users/List.tsx
@@ -0,0 +1,48 @@
+import { useQuery } from 'react-query';
+
+type User = {
+  email: string;
+  id: string;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+  company: {
+    bs: string;
+    catchPhrase: string;
+    name: string;
+  };
+  address: {
+    city: string;
+    street: string;
+    suite: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+};
+
+export default function List() {
+  const { data } = useQuery<User[]>(
+    'users',
+    () =>
+      fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+        res.json(),
+      ),
+    {
+      suspense: true,
+    },
+  );
+
+  return (
+    <ul>
+      {data?.map((user) => (
+        <li key={user.id}>
+          {user.id} - {user.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,9 @@
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import { Suspense } from 'react';
+const List = dynamic(() => import('@/components/users/List'), {
+  ssr: false,
+});
 
 export default function Users() {
   return (
@@ -7,7 +12,22 @@ export default function Users() {
         <title>Users</title>
         <meta name="description" content="Users" />
       </Head>
-      <div>Users</div>
+      <h1>Users</h1>
+      <Suspense
+        fallback={
+          <div
+            css={{
+              width: 100,
+              height: 100,
+              backgroundColor: 'red',
+            }}
+          >
+            Loading..
+          </div>
+        }
+      >
+        <List />
+      </Suspense>
     </>
   );
 }
